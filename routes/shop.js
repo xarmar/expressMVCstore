152ff85c14e04efe0a0fc628b0f97ae57a6ba45b
Xavier Marques <xavierpmarques@gmail.com>
2021-11-01 14:54:48 +0000

    Fix routes in shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -3,14 +3,22 @@ var router = express.Router();
 
 // Require controller modules.
 var category_controller = require("../controllers/categoryController");
+var item_controller = require("../controllers/itemController");
+
 
 /// Category ROUTES ///
 
-// GET shop categories.
-router.get("/", category_controller.category_list);
+// GET - Redirect user from "/" to "/shop"
+router.get("/", category_controller.category_list_get);
 
-// GET category routes.
-router.get("/category/*", category_controller.display_category);
+// GET - Redirect user to category page
+router.get("/category/*", category_controller.category_get);
+
+// GET - Redirect user to edit item page
+// router.get("/item/:id/edit", item_controller.item_edit_get);
+
+// POST - Delete item
+router.post("/item/:id/delete", item_controller.item_delete_post);
 
 
 module.exports = router;
