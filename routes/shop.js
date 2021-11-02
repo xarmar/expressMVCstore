328957ac05b2384e0e40772758aea9a9079ebedb
Xavier Marques <xavierpmarques@gmail.com>
2021-11-02 23:36:33 +0000

    Organize routes

--- routes/shop.js
+++ routes/shop.js
@@ -11,17 +11,22 @@ var item_controller = require("../controllers/itemController");
 // GET - Redirect user from "/" to "/shop"
 router.get("/", category_controller.category_list_get);
 
-// GET - Create Item request
-router.get("/category/*/:id/new", item_controller.item_create_get);
+// GET - Update Item request
+router.get("/category/*/:category/:item/edit", item_controller.item_edit_get)
 
+// Get - Delete Item request
+router.get("/category/*/:category/:item/delete", item_controller.item_delete_get)
+// POST - Delete Item request
+router.post("/category/*/:category/:item/delete", item_controller.item_delete_post)
+
+// GET - Create Item request
+router.get("/category/*/:category/new", item_controller.item_create_get);
 // POST - Create Item request
-router.post("/category/*/:id/new", item_controller.item_create_post)
+router.post("/category/*/*/new", item_controller.item_create_post)
 
 // GET - Read Category request
 router.get("/category/*/:id", category_controller.category_get);
 
-// POST - Delete Item request
-router.post("/category/*/:id/delete", item_controller.item_delete_post);
 
 // GET - Redirect user to edit item page
 // router.get("/item/:id/edit", item_controller.item_edit_get);
