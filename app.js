f8b9bcf60904ef38eb08421eb775eb71c780c207
Xavier Marques <xavierpmarques@gmail.com>
2021-11-08 00:19:39 +0000

    Rename 'shop' router to 'inventory'. Refractor code

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var shopRouter = require('./routes/shop');
+var inventoryRouter = require('./routes/inventory');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/shop', shopRouter);
+app.use('/inventory', inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
