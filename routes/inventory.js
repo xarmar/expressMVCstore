59ceb207864a381c65587c1f10390c429d89edcf
Xavier Marques <xavierpmarques@gmail.com>
2021-11-09 19:11:57 +0000

    Rename item form validator

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const validators = require("../validators/validators");
+const validators = require("../validators/itemFormValidator");
 const multer  = require('multer');
 const upload = multer({ dest: 'public/images' });
 
@@ -16,17 +16,17 @@ router.get("/", category_controller.category_list_get);
 router.get("/category/*/:category/:item/edit", item_controller.item_update_get)
 
 // POST - Update Item request
-router.post("/category/*/:category/:item/edit", upload.single('image'), validators.validateItem, item_controller.item_update_post)
+router.post("/category/*/:category/:item/edit", upload.single('image'), validators.itemFormValidator, item_controller.item_update_post)
 
 // Get - Delete Item request
 router.get("/category/*/:category/:item/delete", item_controller.item_delete_get)
 // POST - Delete Item request
-router.post("/category/*/:category/:item/delete", item_controller.item_delete_post)
+router.post("/category/*/:category/:item/delete", upload.none(), item_controller.item_delete_post)
 
 // GET - Create Item request
 router.get("/category/*/:category/new", item_controller.item_create_get);
 // POST - Create Item request
-router.post("/category/*/:category/new", upload.single('image'), validators.validateItem, item_controller.item_create_post)
+router.post("/category/*/:category/new", upload.single('image'), validators.itemFormValidator, item_controller.item_create_post)
 
 // GET - Read Category request
 router.get("/category/*/:id", category_controller.category_get);
