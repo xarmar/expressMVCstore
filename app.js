ddaee3475843134c99865120461e8c1f37f3c771
Xavier Marques <xavierpmarques@gmail.com>
2021-11-20 20:27:32 +0000

    Add 404.pug template for 404 not found

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/inventory', inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.render("404");
 });
 
 // error handler
